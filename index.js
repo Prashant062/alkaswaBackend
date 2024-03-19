c0a24a134db87d7c94dc0c4081db710e886ca897
Prashant062 <prashantrawat786216@gmail.com>
2024-03-19 09:34:40 +0530

    new changes

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ const __dirname = dirname(__filename);
 const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true, 
+            useNewUrlParser: true
         }); 
         console.log("MongoDB database connected");
     } catch (error) {
